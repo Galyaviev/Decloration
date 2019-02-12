3dbddf4c980076ce5b49b60e0b0036d7964768f1
Ruslan <ruslan@ipcom.su>
2019-02-12 16:52:16 +0300

    version b0.00 update

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import requests
 import telebot
 import json
 import time
+import config
 url = 'https://declarator.org/api/v1/search/person-sections/?name='
 
 
 
 
-bot = telebot.TeleBot('586569609:AAGkb_-IPu3qy9kZX9En2hJGHp-TUaCqqsI')
+bot = telebot.TeleBot(config.tokken)
 
 
 @bot.message_handler(commands=['start'])
@@ -21,42 +22,22 @@ def handle_start(message):
 def send_message(message):
     fio = message.text
     fio = fio.replace(" ","?")
-    chat_id = message.chat.id
-    get_updates(fio)
-
-
-
-def get_updates(fio):
     r = requests.get(url + fio)
-    print(r.encoding)
-
-
-    write_json(r.json())
-
-
-
-
-def convert():
-    with open('answer.json', 'r', encoding='utf-8') as fh:  # открываем файл на чтение
-        data = json.load(fh)  # загружаем из файла данные в словарь data
+    with open('answer.json', 'w', encoding='utf8') as f:
+        json.dump(r.json(), f, indent=2, ensure_ascii=False)
+    with open('answer.json', 'r', encoding='utf-8') as f:  # открываем файл на чтение
+        data = json.load(f)  # загружаем из файла данные в словарь data
         count = data['count']
         if count == 0:
-
-            pass
-
-        position = data['results'][0]['sections'][0]['position']
-        print(position)
-        if position == '':
-            position = data['results'][1]['sections'][0]['position']
-            print(position)
-
+            bot.send_message(message.chat.id, 'Данный гражданин не найден')
+        else:
+            print(count)
+            position = data['results'][0]['sections'][0]['position']
+            if position == '':
+                position = data['results'][1]['sections'][0]['position']
+                print(position)
 
 
-def write_json(data, filename='answer.json'):
-    with open(filename, 'w', encoding='utf8') as f:
-        json.dump(data, f, indent=2, ensure_ascii=False)
-
-    convert()
 
 
 
